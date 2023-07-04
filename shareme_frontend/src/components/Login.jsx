ab0f890d1651b37736f240add47999c161c4bce9
Stephen-Rivera-XIII <sriveraxiii@gmail.com>
2023-07-04 15:42:41 -0500

    OAuth 2 complete same error :'(

--- shareme_frontend/src/components/Login.jsx
+++ shareme_frontend/src/components/Login.jsx
@@ -1,8 +1,57 @@
 import React from 'react'
+import {GoogleLogin, googleLogout} from '@react-oauth/google'
+import { useNavigate } from 'react-router-dom'
+import { FcGoogle } from 'react-icons/fc'
+import shareVideo from '../assets/share.mp4'
+import logo from '../assets/logowhite.png'
 
 const Login = () => {
+
+  const responseGoogle = (response) => {
+    localStorage.setItem('user', JSON.stringify(response.profileObj))
+
+    const { name, googleId, imageUrl } = response.profileObj
+
+    const doc = {
+      _id: googleId,
+      _type: 'user',
+      userName: name,
+      image: imageUrl,
+    }
+  }
   return (
-    <div>Login</div>
+    <div className='flex justify-center items-center flex-col h-screen'>
+      <div className='relative w-full h-full'>
+        <video 
+          src = {shareVideo}
+          type = 'video/mp4'
+          loop
+          controls = {false}
+          muted
+          autoPlay
+          className='w-full h-full object-cover'
+        />
+        <div className='absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay'>
+          <div className='p-5'>
+            <img src={logo} alt='logo' width='130px'  />
+          </div>  
+          <div className='shadow-2xl'>
+            <GoogleLogin 
+              onSuccess={(response) => 
+                console.log(response)}
+              onError={() => {
+                console.log('error')
+              }}
+              clientId={process.env.REACT_APP_GOOGLE_API_TOKEN}>
+              <button className='bg-white text-black font-bold p-3 rounded-full shadow-lg hover:shadow-xl outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150'>
+                <FcGoogle />
+              </button>
+            </GoogleLogin>
+          </div>
+        </div>
+      </div>
+      Login
+    </div>
   )
 }
 
