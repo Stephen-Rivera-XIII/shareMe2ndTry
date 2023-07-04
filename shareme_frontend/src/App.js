4ae457a98a65d85939c3ea0ffc65cb4165fe4982
Stephen-Rivera-XIII <sriveraxiii@gmail.com>
2023-07-04 15:01:33 -0500

    minor correction to boiler plate

--- shareme_frontend/src/App.js
+++ shareme_frontend/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
-export const App = () => {
+const App = () => {
   return (
     <h1 className='text-3xl font-bold underline'>
       Hello world!
     </h1>
   )
 }
+
+export default App;
+
