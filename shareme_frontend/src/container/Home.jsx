744f3708274da0b4f4af65ba743c640e1d6ac79c
Stephen-Rivera-XIII <sriveraxiii@gmail.com>
2023-07-07 09:19:56 -0500

    Navbar complete

--- shareme_frontend/src/container/Home.jsx
+++ shareme_frontend/src/container/Home.jsx
@@ -12,9 +12,9 @@ import jwtDecode from 'jwt-decode';
 
 
 const Home = () => {
-  console.log(localStorage.getItem('user'))
   const [toggleSidebar, setToggleSideBar] = useState(false);
   const [user, setUser] = useState(null);
+  const scrollRef= useRef(null);
 
   // const decodedToken = localStorage.getItem('user') //? jwtDecode(localStorage.getItem('user')) : null;
   const userInfo = localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear();
@@ -28,8 +28,13 @@ const Home = () => {
     });
   }, []);
 
+  useEffect(() => {
+    scrollRef.current.scrollTo(0,0)
+  }, []);
+  
+
   return (
-    <div className='flex bg-gray-50 md:flex-row flex-col h-screen transition-height ease-out duration-75'>
+    <div className='flex bg-gray-50 md:flex-row flex-col h-screen transition-height duration-75 ease-out'>
       <div className='hidden md:flex h-screen flex-initial'>
         <Sidebar user={user && user}/>
       </div>
@@ -38,7 +43,7 @@ const Home = () => {
           <HiMenu
             fontSize={40}
             className='cursor-pointer'
-            onClick={() => setToggleSideBar(false)}
+            onClick={() => setToggleSideBar(true)}
           />
           <Link to='/'>
             <img src={logo} alt='logo' className='w-28' />
@@ -46,11 +51,25 @@ const Home = () => {
           <Link to={`user-profile/${user?._id}`} >
             <img src={user?.image} alt='user-pic' className='w-9 h-9 rounded-full' />
           </Link>
+        </div>
+      </div>
+      {toggleSidebar && (
+        <div className='fixed w-4/5 bg-white h-screen overflow-y-auto shadow-md z-10 animate-slide-in'>
+          <div className='absolute w-full flex justify-end items-center p-2'>
+            <AiFillCloseCircle fontSize={30} className='cursor-pointer' onClick={() => setToggleSideBar(false)}/>
           </div>
-          
+          <Sidebar user={user && user} closeToggle={setToggleSideBar}/>
+        </div>
+      )}
+      <div className='pb-2 flex-1 h-screen overflow-y-scroll' ref={scrollRef}>
+        <Routes>
+          <Route path= "/user-profile/:userId" element={<UserProfile />}/>
+          <Route path= "/*" element={<Pins user={user && user}/>}/>
+        </Routes>
       </div>
     </div>
   );
+  
 };
 
 export default Home;
