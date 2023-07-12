e83ccd44e890c250b25d50f1f4afd92036c7b137
Stephen-Rivera-XIII <sriveraxiii@gmail.com>
2023-07-11 19:16:52 -0500

    Create and Save Pin complete. TODOs added

--- shareme_frontend/src/components/CreatePin.jsx
+++ shareme_frontend/src/components/CreatePin.jsx
@@ -13,7 +13,7 @@ const CreatePin = ({ user }) => {
   const [destination, setDestination] = useState('')
   const [loading, setLoading] = useState(false)
   const [fields, setFields] = useState(null)
-  const [category, setcategory] = useState(null)
+  const [category, setCategory] = useState(null)
   const [imageAsset, setImageAsset] = useState(null)
   const [wrongImageType, setWrongImageType] = useState(false)
 
@@ -40,6 +40,41 @@ const CreatePin = ({ user }) => {
       setWrongImageType(true)
     }
   }
+
+  const savePin = () =>{
+    if(title && about && destination && imageAsset?._id && category){
+      const doc = {
+        _type: 'pin',
+        title,
+        about,
+        destination,
+        image: {
+          _type: 'image',
+          asset: {
+            _type: 'reference',
+            _ref: imageAsset?._id,
+          },
+        },
+        userId: user._id,
+        postedBy: {
+          _type: 'postedBy',
+          _ref: user._id,
+        },
+        // save: [],
+        category,
+      }
+      client.create(doc)
+      .then(()=>{
+        navigate('/')
+      })
+    }
+      else{
+        setFields(true)
+        
+        setTimeout(()=> setFields(false), 2000)
+      }
+    }
+  
 
   return (
     <div className='flex flex-col justify-center items-center mt-5 lg:h-4/5'>
@@ -70,10 +105,86 @@ const CreatePin = ({ user }) => {
                 />
               </label>
             ) : (
-              // Replace this placeholder with the desired content or component TODO: start from here
-              <p>Image preview</p>
+              <div className='relative h-full'>
+                <img src= {imageAsset?.url} alt='uploaded-pic' className='h-full w-full' />
+                <button
+                  type='button'
+                  className='absolute bottom-3 right-3 p-3 rounded-full bg-white text-xl cursor-pointer outline-none hover:shadow-md transition-all duration-500 ease-in-out'
+                  onClick={()=>setImageAsset(null)}
+                >
+                  <MdDelete/>
+                </button>
+              </div>
             )}
           </div>
+        </div>
+
+        <div className='flex flex-1 flex-col gap-6 lg:pl-5 mt-5 w-full'>
+              <input 
+                type='text'
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                placeholder='Add your title here'
+                className='outline-none text-2xl sm:text-3xl font-bold border-b-2 border-gray-200 p-2'
+              />
+              {user &&(
+                <div className='flex gap-2 my-2 items-center bg-white rounded-lg'>
+                  <img
+                    src={user.image}
+                    className='w-10 h-10 rounded-full '
+                    alt='user-profile'
+                  />
+                  <p className='font-bold'>{user.userName}</p>
+                </div>
+              )}
+              <input 
+                type='text'
+                value={about}
+                onChange={(e) => setAbout(e.target.value)}
+                placeholder='What is your pin about'
+                className='outline-none text-base sm:text-lg border-b-2 border-gray-200 p-2'
+              />
+              <input 
+                type='text'
+                value={destination}
+                onChange={(e) => setDestination(e.target.value)}
+                placeholder='Add a destination link'
+                className='outline-none text-base sm:text-lg border-b-2 border-gray-200 p-2'
+              />
+              <div className='flex flex-col'>
+              <div>
+              <p className="mb-2 font-semibold text-lg sm:text-xl">Choose Pin Category</p>
+              <select
+                onChange={(e) => setCategory(e.target.value)}
+                className='outline-none w-4/5 text-base border-b-2 border-gray-200 p-2'
+              >
+                <option value="others" className='sm:text-bg bg-white'>
+                  Select Category
+                </option>
+                {categories.map((category) => (
+                  <option
+                    key={category._id} 
+                    className="text-base border-0 outline-none capitalize bg-white text-black "
+                    value={category.name}
+                  > 
+                    {category.name}
+                  </option>
+                ))}
+                {/* TODO: Since the category names aren't capitalized by tailwind, the images aren't being saved to the categories*/}
+                {/* TODO: Whenever the sanity db is manually updated, the images duplicate. I changed the category for one wall paper and now I see it twice, even tho I've changed it back since */}
+              </select>
+
+            </div>
+            <div className='flex justify-end items-end mt-5'>
+                  <button
+                    type='button'
+                    onClick={savePin}
+                    className='bg-red-500 text-white font-bold p-2 rounded-full w-28 outline-none'
+                  >
+                    Save Pin
+                  </button>
+            </div>
+          </div> 
         </div>
       </div>
     </div>
