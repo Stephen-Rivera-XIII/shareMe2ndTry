9ec7295360270839575bb16fccbd2d9d704d7e5e
Stephen-Rivera-XIII <sriveraxiii@gmail.com>
2023-07-13 20:11:01 -0500

    Added debounced search

--- shareme_frontend/src/components/Search.jsx
+++ shareme_frontend/src/components/Search.jsx
@@ -1,42 +1,46 @@
-import React, { useState, useEffect} from 'react'
-
-import MasonryLayout from './MasonryLayout'
+import React, { useState, useEffect } from 'react';
+import MasonryLayout from './MasonryLayout';
 import { client } from '../client';
 import { feedQuery, searchQuery } from '../utils/data';
-import Spinner from './Spinner'
+import Spinner from './Spinner';
+import { debounce } from 'lodash';
 
-const Search = ({searchTerm}) => {
-  const [pins, setPins] = useState(null)
-  const [loading, setLoading] = useState(false)
+const Search = ({ searchTerm }) => {
+  const [pins, setPins] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const searchPins = debounce((term) => {
+    setLoading(true);
+    const query = searchQuery(term.toLowerCase());
+    client.fetch(query).then((data) => {
+      setPins(data);
+      setLoading(false);
+    });
+  }, 500); // Adjust the debounce delay as needed (e.g., 500 milliseconds)
 
   useEffect(() => {
-    if(searchTerm){
-      setLoading(true)
-      const query = searchQuery(searchTerm.toLowerCase())
-      client.fetch(query)
-      .then((data) => {
-        setPins(data)
-        setLoading(false)
-      })
-    }else{
-      client.fetch(feedQuery)
-      .then((data) => {
-        setPins(data)
-        setLoading(false)
-      })
+    if (searchTerm) {
+      searchPins(searchTerm);
+    } else {
+      setLoading(true);
+      client.fetch(feedQuery).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
     }
-  }, [searchTerm])
-  
+  }, [searchTerm]);
 
   return (
     <div>
       {loading && <Spinner message="Searching for pins..." />}
-      {pins?.lenght !== 0 && <MasonryLayout pins={pins} />}
-      {pins?.length === 0 && searchTerm !== '' && !loading(
-        <div className='mt-10 text-center text-xl'></div>
+      {pins?.length !== 0 && <MasonryLayout pins={pins} />}
+      {pins?.length === 0 && searchTerm !== '' && !loading && (
+        <div className="mt-10 text-center text-xl">
+          No pins found, but check these out!
+        </div>
       )}
     </div>
-  )
-}
+  );
+};
 
-export default Search
+export default Search;
